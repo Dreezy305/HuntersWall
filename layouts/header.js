6b33c5989cfa04f25af5d94eb8e02e1f002a33fa
Idris Bankole <bankoleidris@gmail.com>
2021-12-06 23:53:34 +0100

    nprogress on navbar

--- layouts/header.js
+++ layouts/header.js
@@ -1,8 +1,27 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 export default function Header() {
+  const [mobile, setMobile] = useState(false);
+
+  Router.events.on("routeChangeStart", () => {
+    NProgress.set(0.4);
+    NProgress.start();
+  });
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+    setMobile(false);
+  });
+  Router.events.on("routeChangeError", () => NProgress.done());
+
+  const toggle = () => {
+    setMobile(!mobile);
+  };
+
   return (
     <div className="nav">
       <input type="checkbox" id="nav-check" />
@@ -10,10 +29,52 @@ export default function Header() {
         <div className="nav-title">HuntersWall</div>
       </div>
       <div className="nav-btn">
-        <label htmlFor="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
+        <label htmlFor="nav-check" id="nav-icon">
+          {/* <span></span> */}
+          {/* <span></span> */}
+          {/* <span></span> */}
+          {!mobile ? (
+            <svg
+              width="32"
+              height="21"
+              viewBox="0 0 32 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => toggle()}
+            >
+              <rect y="0.120117" width="32" height="4" rx="2" fill="#2081F9" />
+              <rect y="8.44019" width="32" height="4" rx="2" fill="#2081F9" />
+              <rect y="16.76" width="32" height="4" rx="2" fill="#2081F9" />
+            </svg>
+          ) : (
+            <svg
+              width="26"
+              height="25"
+              viewBox="0 0 26 25"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => toggle()}
+            >
+              <rect
+                x="21.2134"
+                y="0.211426"
+                width="4"
+                height="30"
+                rx="2"
+                transform="rotate(45 21.2134 0.211426)"
+                fill="#2081F9"
+              />
+              <rect
+                x="1.16797"
+                y="2.82812"
+                width="4"
+                height="30"
+                rx="2"
+                transform="rotate(-45 1.16797 2.82812)"
+                fill="#2081F9"
+              />
+            </svg>
+          )}
         </label>
       </div>
 
