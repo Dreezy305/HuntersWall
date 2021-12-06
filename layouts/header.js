ebbc03cca7589b53d83a0c22154c9b6ebcaca792
Idris Bankole <bankoleidris@gmail.com>
2021-12-07 00:48:27 +0100

    nprogress on navbar ++ responsiveness

--- layouts/header.js
+++ layouts/header.js
@@ -26,7 +26,7 @@ export default function Header() {
     <div className="nav">
       <input type="checkbox" id="nav-check" />
       <div className="nav-header ms-lg-5">
-        <div className="nav-title">HuntersWall</div>
+        <div className="nav-title nav-headline">HuntersWall</div>
       </div>
       <div className="nav-btn">
         <label htmlFor="nav-check" id="nav-icon">
