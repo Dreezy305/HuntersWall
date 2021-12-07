b2ca93a48ad99336a17cb9315f2edc887203cddb
Idris Bankole <bankoleidris@gmail.com>
2021-12-08 00:45:51 +0100

    asset types

--- module/assetTypes.js
+++ module/assetTypes.js
@@ -1,9 +1,52 @@
 import React from "react";
 
 function AssetTypes() {
+  const assets = {
+    realEstate: "/img/real-estate.svg",
+    commodities: "/img/commodities.svg",
+    innovation: "/img/innovation.svg",
+    portfolio: "/img/diversified-portfolio.svg",
+  };
   return (
-    <section>
-      <h1>Asset Types</h1>
+    <section className="mt-5 assets">
+      <div className="d-flex flex-row justify-content-evenly">
+        <div className="">
+          <img
+            src={assets.realEstate}
+            className=""
+            alt=""
+            style={{ background: "rgba(32, 129, 249, 0.4)" }}
+          />
+          <h6 className="mt-1">Real Estate</h6>
+        </div>
+        <div className="">
+          <img
+            src={assets.commodities}
+            className=""
+            alt=""
+            style={{ background: "rgba(230, 71, 40, 0.4)" }}
+          />
+          <h6 className="mt-1">Commodities</h6>
+        </div>
+        <div className="">
+          <img
+            src={assets.innovation}
+            className=""
+            alt=""
+            style={{ background: "rgba(244, 160, 35, 0.4)" }}
+          />
+          <h6 className="mt-1">Innovation</h6>
+        </div>
+        <div className="">
+          <img
+            src={assets.portfolio}
+            className=""
+            alt=""
+            style={{ background: "rgba(114, 54, 246, 0.25)" }}
+          />
+          <h6 className="mt-1">Diversified Portfolio</h6>
+        </div>
+      </div>
     </section>
   );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AssetTypes from "../module/assetTypes";
 
 function Home() {
   const images = {
@@ -7,26 +8,31 @@ function Home() {
     hunt: "/img/hunt.png",
   };
   return (
-    <section className="home p-2">
-      <div className="container ">
-        <div className="row align-items-stretch gx-5">
-          <div className="col-lg-6 my-5">
-            <h1 className="">
-              <span>We have</span>
-              <span className=""> fractionalized alternative assets</span>
-              <span> for you.</span>
-            </h1>
-            <p>With small amount, equity stake in big assets is now possible</p>
-            <button type="button" className="px-2 p-2">
-              Create an account
-            </button>
-          </div>
-          <div className="col-lg-6 mt-2">
-            <img src={images.hunt} className="img-fluid" alt="hunterswall" />
+    <>
+      <section className="home p-2">
+        <div className="container ">
+          <div className="row align-items-stretch gx-5">
+            <div className="col-lg-6 my-5">
+              <h1 className="">
+                <span>We have</span>
+                <span className=""> fractionalized alternative assets</span>
+                <span> for you.</span>
+              </h1>
+              <p>
+                With small amount, equity stake in big assets is now possible
+              </p>
+              <button type="button" className="px-2 p-2">
+                Create an account
+              </button>
+            </div>
+            <div className="col-lg-6 mt-2">
+              <img src={images.hunt} className="img-fluid" alt="hunterswall" />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <AssetTypes />
+    </>
   );
 }
 
