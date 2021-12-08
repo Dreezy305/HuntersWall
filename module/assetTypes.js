e8c3fb9128b181942fa614ef663521169d0b6f05
Idris Bankole <bankoleidris@gmail.com>
2021-12-08 01:09:15 +0100

    asset types

--- module/assetTypes.js
+++ module/assetTypes.js
@@ -9,42 +9,44 @@ function AssetTypes() {
   };
   return (
     <section className="mt-5 assets">
-      <div className="d-flex flex-row justify-content-evenly">
-        <div className="">
-          <img
-            src={assets.realEstate}
-            className=""
-            alt=""
-            style={{ background: "rgba(32, 129, 249, 0.4)" }}
-          />
-          <h6 className="mt-1">Real Estate</h6>
-        </div>
-        <div className="">
-          <img
-            src={assets.commodities}
-            className=""
-            alt=""
-            style={{ background: "rgba(230, 71, 40, 0.4)" }}
-          />
-          <h6 className="mt-1">Commodities</h6>
-        </div>
-        <div className="">
-          <img
-            src={assets.innovation}
-            className=""
-            alt=""
-            style={{ background: "rgba(244, 160, 35, 0.4)" }}
-          />
-          <h6 className="mt-1">Innovation</h6>
-        </div>
-        <div className="">
-          <img
-            src={assets.portfolio}
-            className=""
-            alt=""
-            style={{ background: "rgba(114, 54, 246, 0.25)" }}
-          />
-          <h6 className="mt-1">Diversified Portfolio</h6>
+      <div className="container">
+        <div className="row text-center justify-content-sm-center assetRow">
+          <div className="col-lg-2 assets_col" style={{ width: "25%" }}>
+            <img
+              src={assets.realEstate}
+              className=""
+              alt=""
+              style={{ background: "rgba(32, 129, 249, 0.4)" }}
+            />
+            <h6 className="mt-1 text-center">Real Estate</h6>
+          </div>
+          <div className="col-lg-2 assets_col" style={{ width: "25%" }}>
+            <img
+              src={assets.commodities}
+              className=""
+              alt=""
+              style={{ background: "rgba(230, 71, 40, 0.4)" }}
+            />
+            <h6 className="mt-1 text-center">Commodities</h6>
+          </div>
+          <div className="col-lg-2 assets_col" style={{ width: "25%" }}>
+            <img
+              src={assets.innovation}
+              className=""
+              alt=""
+              style={{ background: "rgba(244, 160, 35, 0.4)" }}
+            />
+            <h6 className="mt-1 text-center">Innovation</h6>
+          </div>
+          <div className="col-lg-2 assets_col" style={{ width: "25%" }}>
+            <img
+              src={assets.portfolio}
+              className=""
+              alt=""
+              style={{ background: "rgba(114, 54, 246, 0.25)" }}
+            />
+            <h6 className="mt-1">Diversified Portfolio</h6>
+          </div>
         </div>
       </div>
     </section>
