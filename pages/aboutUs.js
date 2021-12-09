2c3a8d75fa8a1dac44cfec8838114b8b9fd17ea7
Idris Bankole <bankoleidris@gmail.com>
2021-12-09 19:20:33 +0100

    about us section

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -1,12 +1,48 @@
 import React from "react";
 
 function AboutUs() {
+  const images = {
+    about: "/img/aboutus.png",
+  };
   return (
     <section className="mt-5 pt-5 aboutUs">
-      <div className="text-center">
+      <div className="text-center aboutUs_content">
         <h1>
-          <span>Are you cash-strapped?</span> <span>We got you!</span>
+          <span>Are you cash-strapped?</span>{" "}
+          <span className="ms-2">We got you!</span>
         </h1>
+        <p className="text-center mx-auto mt-3">
+          <span>Acquired asset on this platform can be used to secure a</span>{" "}
+          <span>loan with monthly interest of 1.2%.</span>{" "}
+          <span>Low interest, right?</span>{" "}
+          <span className="ms-1">We are after your smile</span>
+        </p>
+      </div>
+      <div className="container mt-lg-3 text-center">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-lg-6">
+            <img
+              src={images.about}
+              className="img-fluid py-lg-5"
+              alt="about_us"
+            />
+          </div>
+          <div className="col-lg-4 aboutUs_col2">
+            <h1 className="mb-5">About us</h1>
+            <h6 className="mb-4">
+              We are a team of stategists, researchers and analysts that held
+              strong positions from various fileds such as real estate,
+              manufacturing, technology, and finance. We build and maintain
+              relationships which are based on trust, strategic ideas, results,
+              and exceptional deliveries.
+            </h6>
+            <div className="">
+              <button className="text-light px-4 py-3 float-end">
+                Read more
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
