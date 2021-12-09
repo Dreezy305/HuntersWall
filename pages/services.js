82d1febc62841dcfe56738e3ba73802ac2de8680
Idris Bankole <bankoleidris@gmail.com>
2021-12-09 22:20:10 +0100

    services  section

--- pages/services.js
+++ pages/services.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 function Services() {
+  const serviceTypes = {};
   return (
-    <section className="services">
-      <h1>Services</h1>
+    <section className="services pt-5">
+      <h1 className="text-center">Services</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="card">
+              <div className="card-body"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
