4ddbd5981b2587f24bd9429bb140136a9b685180
Idris Bankole <bankoleidris@gmail.com>
2021-12-09 22:56:11 +0100

    services  section

---
+++ data/services.js
@@ -0,0 +1,33 @@
+export const Services = [
+  {
+    id: 1,
+    title: "Real Estate",
+    content:
+      "It has become a norm to believe a landed property is for the rich. HuntersWall makes it possible for everyone irrespective of your financial worth, to have a fraction of a landed property digitally. These digital assets are backed up by real assets but controlled by the subscribers digitally.",
+    image: "/img/housing.png",
+  },
+  {
+    id: 2,
+    title: "Commodities",
+    content: `All commodities such as agricultural produce which include cocoa, cashew, maize, and
+palm oil; and precious metals which include gold, silver, diamond, and platinum can be acquired and possessed digitally on this platform either by direct buying, or through initial
+offering fund.`,
+    image: "/img/goods.png",
+  },
+  {
+    id: 3,
+    title: "Diversified Portfolio",
+    content:
+      "Our researchers, analysts and strategists work all round the clock, ensuring we invest on safe and low risk asset, with greater reward in return on investment over long term period. We invest in shares of rewarding companies in different industries.",
+    image: "/img/port.png",
+  },
+  {
+    id: 4,
+    title: "Innovation",
+    content: `We commit funds for stake or utility tokens acquisition in innovation companies, but with
+strong commitment and great interest in disruptive-innovation startups. We have thorough
+understanding what disruptive innovations truly are, that why  we position early and take part
+of the great ...`,
+    image: "/img/innovate.png",
+  },
+];
--- pages/services.js
+++ pages/services.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 function Services() {
-  const serviceTypes = {};
+  const serviceTypes = {
+    housing: "/img/housing.png",
+  };
   return (
     <section className="services pt-5">
-      <h1 className="text-center">Services</h1>
+      <h1 className="text-center mb-5">Services</h1>
       <div className="container">
         <div className="row">
-          <div className="col-lg-6">
-            <div className="card">
-              <div className="card-body"></div>
+          <div className="col-lg-6 pt-4">
+            <div className="card px-3 services_card">
+              <img
+                src={serviceTypes.housing}
+                className="card-img-top py-3"
+                alt="real-estate"
+              />
+              <div className="card-body px-1">
+                <h5 className="card-title text-center mb-2">Real Estate</h5>
+                <p className="card-text">
+                  It has become a norm to believe a landed property is for the
+                  rich. HuntersWall makes it possible for everyone irrespective
+                  of your financial worth, to have a fraction of a landed
+                  property digitally. These digital assets are backed up by real
+                  assets but controlled by the subscribers digitally.
+                </p>
+                <button className="px-3 py-2 float-end">Go somewhere</button>
+              </div>
             </div>
           </div>
         </div>
