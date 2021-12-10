b9a8ab9222a8bd0f0f8cdcfd94f6a7d787ea6b3d
Idris Bankole <bankoleidris@gmail.com>
2021-12-10 07:51:15 +0100

    footer section

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Image from "next/image";
-import styles from "../styles/Home.module.scss";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>{/* <h1>Footer goes here</h1> */}</footer>
+    <footer className="footer">
+      <div className="container footer">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-lg-2 footer_col1">
+            <h1 className="">HuntersWall</h1>
+          </div>
+          <div className="col-lg-8 d-lg-flex justify-content-between footer_col2">
+            <h5>Company</h5>
+            <h5>About</h5>
+            <h5>Service</h5>
+            <h5>FAQ</h5>
+            <h5>Contact</h5>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.scss";
+import "../styles/Home.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
