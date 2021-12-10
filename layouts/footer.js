c562c63b6e80b5e080a23f71073c9490910c688b
Idris Bankole <bankoleidris@gmail.com>
2021-12-10 08:04:47 +0100

    footer section

--- layouts/footer.js
+++ layouts/footer.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container footer">
+      <div className="container pt-4">
         <div className="row justify-content-between align-items-center">
           <div className="col-lg-2 footer_col1">
             <h1 className="">HuntersWall</h1>
@@ -16,6 +16,26 @@ function Footer() {
             <h5>FAQ</h5>
             <h5>Contact</h5>
           </div>
+        </div>
+      </div>
+      <hr />
+      <div className="container pt-2">
+        <div className="row footer_p">
+          <p>
+            Even with deep knowledge on risk control, we reiterate that
+            investments are subject to market risk, while some can also be
+            affected by natural disasters. Please read the scheme information
+            and related documents carefully before committing your funds. Past
+            performance is not indicative of future returns. Please consider
+            your specific risk tolerance before choosing a category.
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="container pt-2">
+        <div className="row text-center footer_h1">
+          <h6> Privacy Policy. Terms & Conditions.</h6>
+          <h6>Copyright 2022. Hunterswall Strategies Limited (RC: 1843219)</h6>
         </div>
       </div>
     </footer>
