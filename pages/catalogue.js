7cb766bd5e8dbed09207f02b99f8703ea3abb426
Idris Bankole <bankoleidris@gmail.com>
2021-12-10 16:32:50 +0100

    catalogue  section

--- pages/catalogue.js
+++ pages/catalogue.js
@@ -7,7 +7,7 @@ function Catalogue() {
       <section className="catalogue pt-4">
         <h1 className="text-center catalogue_title">Catalogue</h1>
 
-        <div className=" pt-5 pb-4 d-flex flex-row justify-content-evenly catalogue_row">
+        <div className="pt-5 pb-4 pe-lg-5 d-flex flex-row justify-content-evenly catalogue_row">
           <h6 className="tabs">All</h6>
 
           <h6 className="tabs">Real estate</h6>
