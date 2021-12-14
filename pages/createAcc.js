f377a3797f66af5b03e94fa70743de82da2d50af
Idris Bankole <bankoleidris@gmail.com>
2021-12-14 23:13:26 +0100

    create acc && login page

---
+++ pages/createAcc.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../layouts";
+
+function CreateAcc() {
+  return (
+    <Layout>
+      <h1>Create Account</h1>
+    </Layout>
+  );
+}
+
+export default CreateAcc;
