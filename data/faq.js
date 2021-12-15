eee938134fd5c44d45f75151fc0d0e12ff06a16a
Idris Bankole <bankoleidris@gmail.com>
2021-12-15 11:09:13 +0100

    FAQ

---
+++ data/faq.js
@@ -0,0 +1,14 @@
+export const Faq = [
+  {
+    id: 1,
+    title: `What do we mean by digitalized alternative assets?
+`,
+    content: `
+Our assets fall under alternative category, which include tangible assets like real estate; commodities such as agricultural produce, gold, silver, copper etc.; financial tools such as venture capital, loan, and many more. Their functions depend on the characteristics of each
+kind of asset. For example, real estate investment can be residential for private use to grow
+equity or for rental income; venture capital funding for equity stake; gold/silver acquisition as
+hedge against inflation.
+We digitalize these assets, by allowing Investors to buy fractions of these alternative investments on this platform, not only to have effective hedge against inflation, but also
+generate annual income, as well as possible increased asset value.  Although, this is not risk free, but the reward is greater.`,
+  },
+];
--- pages/faq.js
+++ pages/faq.js
@@ -4,8 +4,8 @@ import Layout from "../layouts";
 function Faq() {
   return (
     <Layout>
-      <section>
-        <h1>FAQ Page</h1>
+      <section className="pt-4 py-3 FAQ">
+        <h1 className="text-center mx-auto py-3 pb-5">FAQ Page</h1>
       </section>
     </Layout>
   );
