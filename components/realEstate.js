26edb6d390ea88283549f7219e9bb14fc1b5f15b
Idris Bankole <bankoleidris@gmail.com>
2021-12-16 00:22:20 +0100

    FAQ page

--- components/realEstate.js
+++ components/realEstate.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 function RealEstate() {
+  const image = {
+    estate: "/img/realestate.png",
+  };
   return (
-    <div>
-      <h1>Real estate</h1>
-    </div>
+    <>
+      <div className="col-lg-6 realEstate_content">
+        <h1 className="pb-3">Asset Code: - REE-076489</h1>
+        <h2 className="pb-5">Description</h2>
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor
+          duis semper feugiat nec imperdiet fames felis. Odio at dui, erat sed.
+          Eu, faucibus posuere iaculis sed auctor dignissim mattis aenean. Diam
+          mauris suscipit vestibulum, morbi. Quisque placerat eget tortor
+          laoreet. Sed pulvinar vitae massa amet, pretium. Massa egestas elit
+          amet fringilla. Urna eget ut feugiat eget sed erat auctor tincidunt
+          arcu. Et pulvinar risus fusce odio tortor varius id dui. Gravida arcu
+          etiam hendrerit at cras faucibus ipsum non habitant. Porttitor amet
+          mollis venenatis velit, odio quis venenatis, feugiat imperdiet. Hac
+          senectus duis urna tortor, ipsum habitant sed. Orci diam rhoncus nibh
+          consectetur ipsum morbi auctor elementum. Viverra in sed ut quis.
+          Iaculis eget sit morbi nisi vulputate malesuada mattis. Congue nisi
+          nisi.
+        </p>
+      </div>
+      <div className="col-lg-6">
+        <img src={image.estate} className="img-fluid" alt="real_estate" />
+      </div>
+    </>
   );
 }
 
--- pages/catalogue.js
+++ pages/catalogue.js
@@ -1,11 +1,21 @@
-import React from "react";
+/* eslint-disable react/jsx-key */
+import React, { useState } from "react";
 import Layout from "../layouts";
 import Card from "../components/card";
 import { All } from "../data/all";
 import AllAssets from "../components/allAssets";
+import RealEstate from "../components/realEstate";
 
 function Catalogue() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [counter, setCounter] = useState(0);
+
+  const catalogues = [
+    <AllAssets />,
+    <RealEstate />,
+    <AllAssets />,
+    <RealEstate />,
+    <AllAssets />,
+  ];
 
   return (
     <Layout>
@@ -13,39 +23,32 @@ function Catalogue() {
         <h1 className="text-center catalogue_title">Catalogue</h1>
         <div className="container">
           <div className="pt-5 pb-4 pe-lg-5 pt-5 pb-4 pe-lg-5 d-lg-flex flex-lg-row justify-content-lg-between catalogue_row d-md-flex flex-md-row justify-content-md-between">
-            <h6 className="tabs">All</h6>
+            <h6 className="tabs" onClick={() => setCounter(0)}>
+              All
+            </h6>
 
-            <h6 className="tabs">Real estate</h6>
+            <h6 className="tabs" onClick={() => setCounter(1)}>
+              Real estate
+            </h6>
 
-            <h6 className="tabs">Innovation</h6>
+            <h6 className="tabs" onClick={() => setCounter(2)}>
+              Innovation
+            </h6>
 
-            <h6 className="tabs">Commodities</h6>
+            <h6 className="tabs" onClick={() => setCounter(3)}>
+              Commodities
+            </h6>
 
-            <h6 className="tabs">Diversified portfolio</h6>
+            <h6 className="tabs" onClick={() => setCounter(4)}>
+              Diversified portfolio
+            </h6>
           </div>
           <div className="d-flex flex-row pb-4 pt-3 mb-4 catalogue_filter">
             <input type="text" className="px-3 py-3" placeholder="Search" />
           </div>
         </div>
         <div className="container pb-5">
-          <div className="row pb-5">
-            {/* <AllAssets /> */}
-            {All.map((item) => (
-              <>
-                <div className="col-lg-4 my-3" key={item.id}>
-                  <Card
-                    AssetCode={item.AssetCode}
-                    assetCode={item.assetCode}
-                    Iov={item.Iov}
-                    AssetClaimTime={item.AssetClaimTime}
-                    subscriptions={item.subscriptions}
-                    offertime={item.offertime}
-                    AssetContent={item.AssetContent}
-                  />
-                </div>
-              </>
-            ))}
-          </div>
+          <div className="row pb-5">{catalogues[counter]}</div>
         </div>
       </section>
     </Layout>
