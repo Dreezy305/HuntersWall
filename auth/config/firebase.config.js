93a35e2e13df84b742cdae4d69b87ea5fe5db7f8
Idris Bankole <bankoleidris@gmail.com>
2021-12-18 08:03:23 +0100

    firebase configuration

--- auth/config/firebase.config.js
+++ auth/config/firebase.config.js
@@ -1,17 +1,21 @@
-// import firebase from "firebase";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/app";
+import "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6XbRT4vbd5QR_5GZDoxxWIc0VqTd4rJY",
-  authDomain: "hunterswall-cf12b.firebaseapp.com",
-  projectId: "hunterswall-cf12b",
-  storageBucket: "hunterswall-cf12b.appspot.com",
-  messagingSenderId: "40369055482",
-  appId: "1:40369055482:web:389163f66a81cf601acb43",
-  measurementId: "G-LW4H5BEYLG",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebaseConfig.appId.length) {
+  firebase.initializeApp(firebaseConfig);
+  if (typeof window !== undefined) {
+    if ("measurementId" in firebaseConfig) {
+      firebase.analytics();
+    }
+  }
+}
