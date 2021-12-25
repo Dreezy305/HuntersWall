02baffca8762810f2cbc4b7017c3e59583e8ae82
Idris Bankole <bankoleidris@gmail.com>
2021-12-25 17:30:44 +0100

    dashboard

--- components/dashboard/rightSide.js
+++ components/dashboard/rightSide.js
@@ -3,9 +3,9 @@ import React from "react";
 function RightSide() {
   return (
     <div className="rightSideBar">
-      <div className="d-flex flex-row justify-content-between">
+      <div className="d-flex flex-row justify-content-between align-items-center rightSideBar_top">
         <h1 className="ms-3">Profile</h1>
-        <div className="me-3">
+        <div className="me-3 d-flex flex-column justify-content-end align-items-end">
           <h6>User Name</h6>
           <p>ID: Hun023475</p>
         </div>
