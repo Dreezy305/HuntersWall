614cbe7feb6844ce17cdd6a4d8b5d2d66f5e42a5
Dreezy <bankoleidris@gmail.com>
2022-01-06 21:53:18 +0100

    profile

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import "../styles/dashboard.scss";
 import "../styles/profile.scss";
 import "../styles/wallet.scss";
 import "../styles/settings.scss";
+import "../styles/form.scss";
 import { AuthProvider } from "../auth/useAuth";
 
 function MyApp({ Component, pageProps }) {
