b1d910b985bdfde6d7c5c19629d6a23eceeadff1
Dreezy <bankoleidris@gmail.com>
2022-01-06 11:29:57 +0100

    top bar and side bar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import SideBar from "../Dashboard/SideBar";
+import TopBar from "../Dashboard/TopBar";
 
 function dashboard() {
   return (
-    <div className="dashboardContainer">
-      <SideBar />
-    </div>
+    <>
+      <TopBar />
+      <div className="dashboardContainer">
+        <SideBar />
+      </div>
+    </>
   );
 }
 
