a86cdac3e921a37d05da8845122eaed9c8428292
Dreezy <bankoleidris@gmail.com>
2022-01-08 00:56:52 +0100

    profile styles

--- components/bankAccountDetails.js
+++ components/bankAccountDetails.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function BankAccountDetails() {
-  return <div className="profile_bankDetails">bank acc details</div>;
+  return (
+    <div className="profile_bankDetails">
+      <div className="profile_personalInfo-wrapper">
+        {/* Account Name */}
+        <div className="row align-items-center profile_personalInfo-names mb-3">
+          <div className="text-start col-lg-6">
+            <label className="form-label pb-2">Account Name</label>
+            <br />
+            <div className="p-3 profileNames">
+              <p>Olagbemi Ifeoluwa</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BankAccountDetails;
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,18 @@
+/* eslint-disable react/jsx-key */
 import React from "react";
 import PersonalInfo from "../components/personalInfo";
+import BankAccountDetails from "../components/bankAccountDetails";
+import { useState } from "react/cjs/react.development";
+import { Tab } from "bootstrap";
 
 function Profile() {
+  const [tab, setTab] = useState(0);
+
+  const Tabs = [
+    { id: 1, component: <PersonalInfo /> },
+    { id: 2, component: <BankAccountDetails /> },
+  ];
+
   return (
     <div className="profile">
       <div className="profile_top d-flex align-items-center justify-content-start">
@@ -9,9 +20,7 @@ function Profile() {
         <h6 className="tabs ms-5">Bank Account Details</h6>
       </div>
 
-      <div className="mt-4">
-        <PersonalInfo />
-      </div>
+      <div className="mt-4">{Tabs[tab].component}</div>
     </div>
   );
 }
