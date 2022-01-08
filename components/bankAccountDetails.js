25f3d4ceb539f43e04fa10a6f59320da63eebb07
Dreezy <bankoleidris@gmail.com>
2022-01-08 01:10:55 +0100

    profile styles

--- components/bankAccountDetails.js
+++ components/bankAccountDetails.js
@@ -36,6 +36,39 @@ function BankAccountDetails() {
             </div>
           </div>
         </div>
+
+        {/* Bank Type */}
+        <div className="row align-items-center profile_personalInfo-names mb-3">
+          <div className="text-start col-lg-6">
+            <label className="form-label pb-2">Account Type</label>
+            <br />
+            <div className="p-3 profileNames">
+              <p>Current</p>
+            </div>
+          </div>
+        </div>
+
+        {/* BVN */}
+        <div className="row align-items-center profile_personalInfo-names mb-3">
+          <div className="text-start col-lg-6">
+            <label className="form-label pb-2">BVN</label>
+            <br />
+            <div className="p-3 profileNames">
+              <p>967233109688288</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Sort code */}
+        <div className="row align-items-center profile_personalInfo-names mb-3">
+          <div className="text-start col-lg-6">
+            <label className="form-label pb-2">Sort Code</label>
+            <br />
+            <div className="p-3 profileNames">
+              <p>09869544</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
