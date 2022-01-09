b04b5707797f3e019536950e130fcd22c2e88bb7
Dreezy <bankoleidris@gmail.com>
2022-01-09 01:43:01 +0100

    nav responsiveness && mobile login

--- pages/wallet.js
+++ pages/wallet.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Wallet() {
-  return <div className="wallet">wallet</div>;
+  return (
+    <div className="wallet">
+      <div className="mt-3">
+        <div className="card wallet_card" style={{}}>
+          <div className="card-body">
+            <h6>Net Worth</h6>
+            <hr />
+            <h6>N105,000.00</h6>
+            <hr />
+            <h6>Cash balance available</h6>
+          </div>
+        </div>
+      </div>
+      <div className="wallet_activity"></div>
+    </div>
+  );
 }
 
 export default Wallet;
