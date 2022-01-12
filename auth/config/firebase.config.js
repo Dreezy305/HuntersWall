db31d1b97dd54e792999b8404e9249ea374871a1
Dreezy <bankoleidris@gmail.com>
2022-01-12 23:03:01 +0100

    remove use auth

--- auth/config/firebase.config.js
+++ auth/config/firebase.config.js
@@ -1,5 +1,5 @@
 // import firebase from "firebase/app";
-import firebase from "firebase/compat/app";
+// import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
