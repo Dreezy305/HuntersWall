818af58f2e832d6faa59c72c5742e1f3c2ed4510
Dreezy <bankoleidris@gmail.com>
2022-01-15 18:46:27 +0100

    payment methods

--- components/deposit.js
+++ components/deposit.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 function Deposit() {
-  const paymentMethods = () => {
-    return <div className="methodContainer"></div>;
+  const paymentMethods = ({ value = "" }) => {
+    return (
+      <div className="methodContainer my-3 d-flex flex-row justify-content-around">
+        <input
+          type="radio"
+          id="oneTime"
+          name="payment"
+          className="radio"
+          value={value}
+        />
+      </div>
+    );
   };
   return (
     <div className="deposit">
@@ -29,6 +39,7 @@ function Deposit() {
                     id="oneTime"
                     name="payment"
                     className="radio"
+                    value="oneTime"
                   />
                   <span className="ms-2 align-top">One Time</span>
                 </label>
@@ -38,6 +49,7 @@ function Deposit() {
                     id="recurring"
                     name="payment"
                     className="radio"
+                    value="recurring"
                   />
                   <span className="ms-2 align-top">Recurring</span>
                 </label>
@@ -45,7 +57,7 @@ function Deposit() {
             </div>
           </div>
 
-          <div className="deposit_paymentMethod">
+          <div className="deposit_paymentMethod mt-4">
             <h6 className="">Choose a Payment Method</h6>
           </div>
         </div>
