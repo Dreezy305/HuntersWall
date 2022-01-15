da84d42ead235c63f958e65d610c5e655bb0cb28
Dreezy <bankoleidris@gmail.com>
2022-01-15 23:40:52 +0100

    withdrawal

--- components/withdrawal.js
+++ components/withdrawal.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Withdrawal() {
-  return <div>Withdrawal</div>;
+  return (
+    <div>
+      <div className="card deposit_card my-4">
+        <div className="card-body text-center">
+          <h5 className="mt-2 mb-3">Deposit</h5>
+
+          <h6 className="mt-3 mb-5">How much do you want to withdraw</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Withdrawal;
