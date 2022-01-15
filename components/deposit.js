295f85063d9c0b97c09d6ef3606d4ec3cc8928e1
Dreezy <bankoleidris@gmail.com>
2022-01-15 20:13:41 +0100

    payment methods, props and button styles

--- components/deposit.js
+++ components/deposit.js
@@ -36,7 +36,7 @@ function Deposit() {
 
   return (
     <div className="deposit">
-      <div className="card deposit_card">
+      <div className="card deposit_card my-4">
         <div className="card-body text-center">
           <h5 className="mt-2 mb-3">Deposit</h5>
           <h6 className="mt-3 mb-5">How much do you want to deposit</h6>
