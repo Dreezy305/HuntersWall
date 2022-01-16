7dc7e84b06796fc6954b88808a0352145dbd3586
Dreezy <bankoleidris@gmail.com>
2022-01-17 00:12:27 +0100

    table

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -13,10 +13,10 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <div className="portfolio_top d-flex align-items-center justify-content-start">
-        <h6 className="tabs me-5 h6" onClick={() => setToggle(0)}>
+        <h6 className="tabs me-5 mb-2 h6" onClick={() => setToggle(0)}>
           Cash value invested
         </h6>
-        <h6 className="tabs ms-5 h6" onClick={() => setToggle(1)}>
+        <h6 className="tabs ms-5 mb-2 h6" onClick={() => setToggle(1)}>
           Asset Performance
         </h6>
       </div>
