3996ba2eded95ac52387759d099c722eb5135a6a
Dreezy <bankoleidris@gmail.com>
2022-01-16 23:08:44 +0100

    table

--- components/cashValue.js
+++ components/cashValue.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { DiversifiedPortfolio } from "../data/cashValueTable";
 
 function CashValue() {
   return (
     <div className="cashValue">
       <h6 className="">Cash value invested per Asset</h6>
 
-      <div className="container">
+      <div className="container mt-4">
         <div className="row">
           <table className="table">
             <thead>
@@ -19,17 +20,17 @@ function CashValue() {
 
             <tbody>
               <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
+                <td>DIV-076489: N50,000</td>
+                <td>REE-890352: N40,000</td>
+                <td>INN-087543: N120,000</td>
+                <td>COM-896369: N43,000</td>
               </tr>
 
               <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
+                <td>DIV-635423: N30,000</td>
+                <td>REE-056003: N90,000</td>
+                <td>INN-789232: N40,000</td>
+                <td>COM-109321: N93,000</td>
               </tr>
 
               <tr>
