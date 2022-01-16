b5169c0a14d3f14a2ba36ee06cd95faeb966e1af
Dreezy <bankoleidris@gmail.com>
2022-01-17 00:33:02 +0100

    table, assets

--- components/assetPerformance.js
+++ components/assetPerformance.js
@@ -7,12 +7,14 @@ function AssetPerformance() {
         <div className="card-body">
           <h5 className="mb-4">Asset code: DIV-076489</h5>
 
-          <h6 className="mt-2">Asset Market: Divisified Portfolio (DIV)</h6>
+          <h6 className="mt-2 mb-3">
+            Asset Market: Diversified Portfolio (DIV)
+          </h6>
 
-          <div className="set d-flex flex-row justify-content-end align-items-center">
+          <div className="set mt-5 d-flex flex-row justify-content-end align-items-center">
             <div className="set_1" />
-            <span>DIV</span>
-            <div />
+            {/* <span>DIV</span> */}
+            <div className="set_1" />
           </div>
         </div>
       </div>
