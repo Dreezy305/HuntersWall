474e25a10e7dab20c208ee4186e9465e2440279f
Dreezy <bankoleidris@gmail.com>
2022-01-17 00:26:10 +0100

    table, assets

--- components/assetPerformance.js
+++ components/assetPerformance.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function AssetPerformance() {
-  return <div>asset performance</div>;
+  return (
+    <div className="assetPerform">
+      <div className="card assetPerform_card" style={{}}>
+        <div className="card-body">
+          <h5 className="mb-4">Asset code: DIV-076489</h5>
+
+          <h6 className="mt-2">Asset Market: Divisified Portfolio (DIV)</h6>
+
+          <div className="set d-flex flex-row justify-content-end align-items-center">
+            <div className="set_1" />
+            <span>DIV</span>
+            <div />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AssetPerformance;
