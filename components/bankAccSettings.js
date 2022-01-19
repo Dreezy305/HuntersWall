41c218194a7b84bbb0c479aeb8eb4dc008358e3e
Dreezy <bankoleidris@gmail.com>
2022-01-19 22:39:14 +0100

    settings page, personal settings and bank acc settings

--- components/bankAccSettings.js
+++ components/bankAccSettings.js
@@ -3,14 +3,24 @@ import React from "react";
 function BankAccSettings() {
   return (
     <div className="bankAccSettings">
-      Bank Account Settings
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <form
-              className="row g-4 py-5"
-              onSubmit={(e) => e.preventDefault()}
-            ></form>
+            <form className="row g-4 py-5" onSubmit={(e) => e.preventDefault()}>
+              {/* FIRST NAME */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">First Name</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
