896917dfa18a78f47fd8741158cc9796556631e9
Dreezy <bankoleidris@gmail.com>
2022-01-19 22:38:09 +0100

    settings page, personal settings and bank acc settings

--- components/bankAccSettings.js
+++ components/bankAccSettings.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function BankAccSettings() {
-  return <div className="bankAccSettings">Bank Account Settings</div>;
+  return (
+    <div className="bankAccSettings">
+      Bank Account Settings
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <form
+              className="row g-4 py-5"
+              onSubmit={(e) => e.preventDefault()}
+            ></form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BankAccSettings;
