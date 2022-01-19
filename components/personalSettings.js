3ecc5a1b064fee2fb2c6543d258c75cb96999663
Dreezy <bankoleidris@gmail.com>
2022-01-19 21:38:21 +0100

    settings page, personal settings and bank acc settings

--- components/personalSettings.js
+++ components/personalSettings.js
@@ -6,7 +6,37 @@ function PersonalSettings() {
       Persal settings
       <div className="container">
         <div className="row">
-          <div className="col-lg-6 border-end">col 1</div>
+          <div className="col-lg-6 border-end">
+            <form className="row g-4 py-5" onSubmit={(e) => e.preventDefault()}>
+              {/* FIRST NAME */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">First Name</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* LAST NAME */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">Last Name</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+            </form>
+          </div>
           <div className="col-lg-6 border-start">col 2</div>
         </div>
       </div>
