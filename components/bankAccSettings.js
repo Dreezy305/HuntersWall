bfa1c13a7d960fb06cf92a32f55f044236fa92d8
Dreezy <bankoleidris@gmail.com>
2022-01-19 22:45:07 +0100

    settings page, personal settings and bank acc settings

--- components/bankAccSettings.js
+++ components/bankAccSettings.js
@@ -9,13 +9,13 @@ function BankAccSettings() {
             <form className="row g-4 py-5" onSubmit={(e) => e.preventDefault()}>
               {/* ACC NAME */}
               <div className="d-flex justify-content-between align-items-baseline">
-                <label className="form-label pb-2 me-3 label">
+                <label className="form-label pb-2 me-1 label">
                   Account Name
                 </label>
                 <br />
                 <input
                   type="text"
-                  className="ms-3 w-75 input"
+                  className="ms-1 w-75 input"
                   value=""
                   autoComplete="on"
                   autoFocus
@@ -39,9 +39,51 @@ function BankAccSettings() {
 
               {/* ACC NUMBER */}
               <div className="d-flex justify-content-between align-items-baseline">
-                <label className="form-label pb-2 me-3 label">
+                <label className="form-label pb-2 me-1 label">
                   Account Number
                 </label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-1 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* BANK TYPE */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">Bank Type</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* BVN */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">BVN</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* SORT CODE */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">Sort Code</label>
                 <br />
                 <input
                   type="text"
