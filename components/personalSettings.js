122712a280714c4ae44ef4f98d485f5b2a7261dd
Dreezy <bankoleidris@gmail.com>
2022-01-19 21:23:58 +0100

    settings page, personal settings and bank acc settings

--- components/personalSettings.js
+++ components/personalSettings.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function PersonalSettings() {
-  return <div className="personalSettings">Personal Settings</div>;
+  return (
+    <div className="personalSettings">
+      Persal settings
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 border-end">col 1</div>
+          <div className="col-lg-6 border-start">col 2</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PersonalSettings;
