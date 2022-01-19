66be436da78eec078104e39022456312d856a9e7
Dreezy <bankoleidris@gmail.com>
2022-01-19 22:40:58 +0100

    settings page, personal settings and bank acc settings

--- components/bankAccSettings.js
+++ components/bankAccSettings.js
@@ -7,9 +7,41 @@ function BankAccSettings() {
         <div className="row">
           <div className="col-lg-6">
             <form className="row g-4 py-5" onSubmit={(e) => e.preventDefault()}>
-              {/* FIRST NAME */}
+              {/* ACC NAME */}
               <div className="d-flex justify-content-between align-items-baseline">
-                <label className="form-label pb-2 me-3 label">First Name</label>
+                <label className="form-label pb-2 me-3 label">
+                  Account Name
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* BANK NAME */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">Bank Name</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75 input"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* ACC NUMBER */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3 label">
+                  Account Number
+                </label>
                 <br />
                 <input
                   type="text"
