b60c8a9505c164b35ce04f859a5ce4f2cb55e1d6
Dreezy <bankoleidris@gmail.com>
2022-01-19 21:44:27 +0100

    settings page, personal settings and bank acc settings

--- components/personalSettings.js
+++ components/personalSettings.js
@@ -49,6 +49,90 @@ function PersonalSettings() {
                   required
                 />
               </div>
+
+              {/* GENDER */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">GENDER</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* BIRTHDAY */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">BIRTHDAY</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* PROFESSION */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">PROFESSION</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* ADDRESS */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">ADDRESS</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* CITY */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">CITY</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
+
+              {/* COUNTRY */}
+              <div className="d-flex justify-content-between align-items-baseline">
+                <label className="form-label pb-2 me-3">COUNTRY</label>
+                <br />
+                <input
+                  type="text"
+                  className="ms-3 w-75"
+                  value=""
+                  autoComplete="on"
+                  autoFocus
+                  required
+                />
+              </div>
             </form>
           </div>
           <div className="col-lg-6 border-start">col 2</div>
